280fcbb9e753351c85512765dfb8b315b7786aee
Donald Sepeng <donaldsepeng@gmail.com>
2020-10-23 21:42:07 +0200

    Fixed the overflowing name on the Person Details page, which was caused by the Hero widget"

--- lib/screens/person_detail.dart
+++ lib/screens/person_detail.dart
@@ -27,83 +27,93 @@ class _PersonDetailState extends State<PersonDetail> {
       body: ListView(
         scrollDirection: Axis.vertical,
         children: <Widget>[
-          Hero(
-            tag: '${widget.actor.id}',
-            child: Material(
-              color: Colors.transparent,
-              child: Container(
-                height: 400,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: (widget.actor.profilePath != null)
-                        ? NetworkImage(
-                            'https://image.tmdb.org/t/p/w780${widget.actor.profilePath}')
-                        : AssetImage(
-                            'assets/images/blank_user.png',
-                          ),
-                    fit: BoxFit.cover,
-                    alignment: Alignment.center,
-                  ),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Row(
+          Stack(
+            children: [
+              Hero(
+                tag: '${widget.actor.id}',
+                child: Material(
+                  color: Colors.transparent,
+                  child: Container(
+                    height: 400,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: (widget.actor.profilePath != null)
+                            ? NetworkImage(
+                                'https://image.tmdb.org/t/p/w780${widget.actor.profilePath}')
+                            : AssetImage(
+                                'assets/images/blank_user.png',
+                              ),
+                        fit: BoxFit.cover,
+                        alignment: Alignment.center,
+                      ),
+                    ),
+                    child: Column(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        IconButton(
-                          icon: Icon(Icons.arrow_back),
-                          color: Colors.white,
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            IconButton(
+                              icon: Icon(Icons.arrow_back),
+                              color: Colors.white,
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                            ),
+                          ],
                         ),
                       ],
                     ),
-                    Container(
-                      padding: EdgeInsets.all(20),
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          begin: Alignment.bottomCenter,
-                          end: Alignment.topCenter,
-                          colors: <Color>[
-                            Color(0xcc000000),
-                            Color(0xaa000000),
-                            Color(0x99000000),
-                            Color(0x66000000),
-                            Color(0x00000000)
-                          ],
-                        ),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                widget.actor.name,
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              // Text(
-                              //   widget.actor.birthday ?? '',
-                              //   style: TextStyle(
-                              //     color: Colors.white70,
-                              //   ),
-                              // )
-                            ],
-                          )
+                  ),
+                ),
+              ),
+              Positioned.fill(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    height: 70,
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.topCenter,
+                        colors: <Color>[
+                          Color(0xcc000000),
+                          Color(0xaa000000),
+                          Color(0x99000000),
+                          Color(0x66000000),
+                          Color(0x00000000)
                         ],
                       ),
                     ),
-                  ],
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              widget.actor.name,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            // Text(
+                            //   widget.actor.birthday ?? '',
+                            //   style: TextStyle(
+                            //     color: Colors.white70,
+                            //   ),
+                            // )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
                 ),
               ),
-            ),
+            ],
           ),
           FutureBuilder(
             future: BackEndService().bundlePersonData(widget.actor.id),
